1bc993bc5488d88ac7d9bdabf65a7fed75ce17ce
Guillermo Delaney <guillermo.delaney_external@wartsila.com>
2024-12-16 14:39:21 +0100

    Adding two way binding with useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useRef, useState } from "react";
 export default function Player() {
+  const inputName = useRef();
+
+  const [enteredPlayerName, setPlayerName ]= useState(null)
+
+  function handleClick(){
+    setPlayerName(inputName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
