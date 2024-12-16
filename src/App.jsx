7a200e09f68f4dcfd6dbf9496bf7088a3ea74163
Guillermo Delaney <guillermo.delaney_external@wartsila.com>
2024-12-16 14:56:40 +0100

    adding basic TimerChallenge.jsx component and binding it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}></TimerChallenge>
+        <TimerChallenge title="Not Easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Getting Tough" targetTime={10}></TimerChallenge>
+        <TimerChallenge title="Expert" targetTime={15}></TimerChallenge>
+
+      </div>
     </>
   );
 }
